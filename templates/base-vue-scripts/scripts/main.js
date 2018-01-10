a1d9bed39dfd1763f348bed012f8dcbd86ded160
Peter Cosemans <peter.cosemans@gmail.com>
2018-01-10 15:13:07 +0100

    Added ESM vue template

---
+++ templates/base-vue-scripts/scripts/main.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-new */
+
+(function() {
+  'use strict';
+
+  new Vue({
+    el: '#app',
+    data() {
+      return {
+        message: 'Hello Vue!',
+      };
+    },
+  });
+})();
